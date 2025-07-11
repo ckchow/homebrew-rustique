29dfff02e58f89edce2e97bd96dc3c65f9832cde
Chris Chow <chris.kr.chow@gmail.com>
2025-07-11 14:12:00 -0700

    add basic formula

---
+++ Formula/r/rustique.rb
@@ -0,0 +1,30 @@
+class Rustique < Formula
+  desc "Vintage Story mod manager in rust."
+  homepage "https://github.com/Tekunogosu/Rustique"
+  url "https://github.com/Tekunogosu/Rustique/archive/refs/tags/v0.5.9.tar.gz"
+  sha256 "ce450a649032e4c13e966619ffeec85029514cb72e02767d71d3532b49973133"
+  license "MIT"
+
+  depends_on "rustup" => :build
+
+  def install
+    # once upstream adds a toolchain lock, this won't be necessary
+    # system "cargo","+nightly-2025-06-23", "build", "--release"
+    # system "cargo", "build", "--release"
+    system "cargo", "install", *std_cargo_args
+   
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test rustique`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system bin/"program", "do", "something"`.
+    system bin/rustique, help
+  end
+end
